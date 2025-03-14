40fd135e728631e65696de7472959e5dee93e62e
DiannaaaMmrrr <murlaevadiana@gmail.com>
2025-03-14 21:27:25 +0300

    update project

--- handle_request.py
+++ handle_request.py
@@ -1,26 +1,43 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from typing import Tuple, Any
+import os
 
 hostName = "localhost"
 serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/main.html'
 
-    def do_get(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))
+        try:
+            file_path = os.path.join(os.getcwd(), self.path[1:])
+            with open(file_path, 'rb') as file:
+                content = file.read()
+            self.send_response(200)
+            if self.path.endswith('.html'):
+                self.send_header('Content-type', 'text/html')
+            elif self.path.endswith('.css'):
+                self.send_header('Content-type', 'text/css')
+            self.end_headers()
+            self.wfile.write(content)
+        except FileNotFoundError:
+            self.send_error(404, f'File Not Found: {self.path}')
+
+
+def create_handler(*args: Tuple[Any, ...]) -> BaseHTTPRequestHandler:
+    return MyServer(*args)
 
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    webServer.get_request()
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    webServer = HTTPServer((hostName, serverPort), create_handler)
+    print(f"Server started http://{hostName}:{serverPort}")
 
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
+
     webServer.server_close()
     print("Server stopped.")
